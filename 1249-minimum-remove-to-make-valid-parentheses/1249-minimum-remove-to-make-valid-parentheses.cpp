e19f92bcc3f4587660d83423232e2cc1b2e776e1
Giusex04 <72347627+Giuseppe-code@users.noreply.github.com>
2024-04-07 02:08:04 +0200

    Time: 17 ms (84.26%), Space: 13.5 MB (33.38%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+    int countA=0;// (
+    int countB=0;// )
+    int index=0;
+
+    vector<int> posA;
+    do{
+        if(s.at(index) == ')')
+            countB++;
+        else if(s.at(index) == '(') {
+            countA++;
+            posA.push_back(index);
+        }
+        if(countB>countA){
+            s.erase(index, 1);
+            countB--;
+            index--;
+        }
+        index++;
+    }while(index < s.size());
+    int rangeDel= countA-countB;
+    for (int i=0; i<rangeDel; i++) {
+        s.erase(posA.back(),1);
+        posA.pop_back();
+    }
+    return s;
+    }
+};
